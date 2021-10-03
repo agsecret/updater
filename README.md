bbcd23ed073a6336ca2e49bd0643ba0fe73b169a
as09 <contact@agent-secret.org>
2021-10-03 18:04:16 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # System & Nextcloud Updater
 Update Nextcloud version, apps, os packages, backup DB, renew TLS 
+
+# crontab
+20 20 * * * /home/scripts/updater.sh 2>&1 | /usr/bin/mail -s "Updater Result" "email@example.com"
+
---
+++ updater.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+
+ncpath='/var/www/nextcloud'
+htuser='www-data'
+htgroup='www-data'
+rootuser='root'
+pass='MYSQL_PASS'
+bkpdir='/home/username/dump/'
+
+printf "\n##-- uptime\n"
+uptime
+
+printf "\n##-- Backup Databases\n"
+mysqldump -u root -p${pass} --default-character-set=utf8mb4 nextcloud > ${bkpdir}nextcloud_"$(date +"%Y-%m-%d")".sql
+
+printf "\n##-- Delete DB dump older then 15 days\n"
+find ${bkpdir} -mtime +15 -exec rm {} \;
+
+printf "\n##-- Backup Nextcloud config files\n"
+sudo tar -cpzvf ${bkpdir}nextcloud-config.tar.gz /var/www/nextcloud/config
+sudo chown "$USER:$USER" ~/dump/nextcloud-config.tar.gz
+
+printf "\n##-- Chown to allow update\n"
+sudo chown -R ${htuser}:${htgroup} ${ncpath}
+
+printf "\n##-- Update the Nextcloud version\n"
+sudo -u www-data php /var/www/nextcloud/updater/updater.phar --no-interaction
+
+printf "\n##-- chmod to secure Files and Directories\n"
+sudo find ${ncpath}/ -type f -print0 | sudo xargs -0 chmod 0640
+sudo find ${ncpath}/ -type d -print0 | sudo xargs -0 chmod 0750
+
+printf "\n##-- chown Directories\n"
+sudo chown -R ${rootuser}:${htgroup} ${ncpath}
+sudo chown -R ${htuser}:${htgroup} ${ncpath}/apps/
+sudo chown -R ${htuser}:${htgroup} ${ncpath}/core/
+sudo chown -R ${htuser}:${htgroup} ${ncpath}/config/
+sudo chown -R ${htuser}:${htgroup} ${ncpath}/data/
+sudo chown -R ${htuser}:${htgroup} ${ncpath}/themes/
+sudo chown -R ${htuser}:${htgroup} ${ncpath}/updater/
+sudo chmod +x ${ncpath}/occ
+
+printf "\n##-- Secure .htaccess\n"
+if [ -f ${ncpath}/.htaccess ]
+ then
+  sudo chmod 0644 ${ncpath}/.htaccess
+  sudo chown ${rootuser}:${htgroup} ${ncpath}/.htaccess
+fi
+if [ -f ${ncpath}/data/.htaccess ]
+ then
+  sudo chmod 0644 ${ncpath}/data/.htaccess
+  sudo chown ${rootuser}:${htgroup} ${ncpath}/data/.htaccess
+fi
+
+printf "\n##-- Update Nextcloud apps\n"
+sudo -u ${htuser} php ${ncpath}/occ app:update --all
+
+printf "\n##-- Remove old unnecessary packages with apt autoremove\n\n"
+DEBIAN_FRONTEND=noninteractive sudo apt-get autoremove
+
+printf "\n##-- Update all OS package with apt\n"
+sudo apt-get update
+sudo apt-get -y upgrade
+
+printf "\n##-- Renew Let's enscrypt TLS if necessary\n"
+sudo /usr/bin/certbot -q renew
+
+if [ -f /var/run/reboot-required ]; then
+        more /var/run/reboot-required
+	echo "System full update and will reboot"
+        sudo reboot
+fi
+printf "\n##-- Update complete\n"
