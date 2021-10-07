2044bf77516a07ee58854a3a9681e72ed0799553
as01p <contact@agent-secret.org>
2021-10-07 05:53:23 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -18,8 +18,8 @@ printf "\n##-- Delete DB dump older then 15 days\n"
 find ${bkpdir} -mtime +15 -exec rm {} \;
 
 printf "\n##-- Backup Nextcloud config files\n"
-sudo tar -cpzvf ${bkpdir}nextcloud-config"$(date +"%Y-%m-%d")".tar.gz ${ncpath}/config
-sudo chown "$USER:$USER" ${bkpdir}nextcloud-config"$(date +"%Y-%m-%d")".tar.gz
+sudo tar -cpzvf ${bkpdir}nextcloud-config_"$(date +"%Y-%m-%d")".tar.gz ${ncpath}/config
+sudo chown "$USER:$USER" ${bkpdir}nextcloud-config_"$(date +"%Y-%m-%d")".tar.gz
 
 printf "\n##-- Chown to allow update\n"
 sudo chown -R ${htuser}:${htgroup} ${ncpath}
